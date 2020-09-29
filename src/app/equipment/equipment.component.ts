033afa9f225eb54e21296adcfa9c478cf8b90f7d
Cierra <cierrajwashi@gmail.com>
2020-09-29 11:12:54 -0400

    modify private field name

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import {EquipmentService} from "./equipment.service";
-import { EquipmentData} from "../backend-interceptor/EquipmentData";
+import { Component, OnInit } from "@angular/core";
+import { EquipmentService } from "./equipment.service";
+import { EquipmentData } from "../backend-interceptor/EquipmentData";
 
 @Component({
-    selector: 'app-equipment',
-    templateUrl: './equipment.component.html',
-    styleUrls: ['./equipment.component.css']
+    selector: "app-equipment",
+    templateUrl: "./equipment.component.html",
+    styleUrls: ["./equipment.component.css"],
 })
 export class EquipmentComponent implements OnInit {
-
     public equipment: EquipmentData[] = [];
     public errorMessage;
-    constructor(private _employeeService: EquipmentService) {}
+    constructor(private _equipmentService: EquipmentService) {}
 
-    ngOnInit(){
-        this._employeeService.getEquipment()
-            .subscribe((data: EquipmentData[]) => this.equipment = data, error=> this.errorMessage = `${error.message}, (error retrieving equipment)`)
+    ngOnInit() {
+        this._equipmentService.getEquipment().subscribe(
+            (data: EquipmentData[]) => (this.equipment = data),
+            (error) =>
+                (this.errorMessage = `${error.message}, (error retrieving equipment)`)
+        );
     }
-};
+}
